fa5e9de1d2991a6f5e2fa3cc3ea9ebf7893c09aa
Aniket Das <anuanshu0309@gmail.com>
2023-12-29 13:36:29 -0600

    Data Transformation Completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+#Import the following Classes from data_transformation file after writing the code in it
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass       #Use this decorator only if you want to define variables in the class
 class DataIngestionConfig:
     train_data_path: str=os.path.join("artifacts","train.csv")          #The output of data ingestion will be stored in "train.csv"
@@ -48,4 +52,7 @@ class DataIngestion:
 #In order to check whether the above code is working fine or not, Use the below code:
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
